9eeb8a82c7dfef83d7743c1854e4dd249198e1f2
Kyle Lee <kylevlee@outlook.com>
2016-12-01 04:22:18 +0800

    Updated project files

--- APCMegaphone/app/src/main/java/apc/lee/kyle/apcmegaphone/LoginActivity.java
+++ APCMegaphone/app/src/main/java/apc/lee/kyle/apcmegaphone/LoginActivity.java
@@ -42,19 +42,12 @@ public class LoginActivity extends AppCompatActivity {
         inputEmail = (EditText) findViewById(R.id.email);
         inputPassword = (EditText) findViewById(R.id.password);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        btnSignup = (Button) findViewById(R.id.btn_signup);
         btnLogin = (Button) findViewById(R.id.btn_login);
         btnReset = (Button) findViewById(R.id.btn_reset_password);
 
         //Get Firebase auth instance
         auth = FirebaseAuth.getInstance();
 
-        btnSignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
-            }
-        });
 
         btnReset.setOnClickListener(new View.OnClickListener() {
             @Override
